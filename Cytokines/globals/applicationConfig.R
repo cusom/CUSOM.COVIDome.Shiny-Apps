0df4b79bd10f93169c69aa8289f9f28d2d046a01
Bartsch <kyle.bartsch@cuanschutz.edu>
2020-12-01 09:27:39 -0700

    Updating app config - swapping out config files for tsv's. Removing un-necessary configs

--- Cytokines/globals/applicationConfig.R
+++ Cytokines/globals/applicationConfig.R
@@ -3,19 +3,19 @@ storeWarn <- getOption("warn")
 options(warn = -1)
 
 # get App metadata
-appConfig <- read_tsv('./config/appConfig.tsv')
+appConfig <- read_tsv('./config/appConfig.tsv',col_types = cols())
 
 ApplicationURL <- ifelse(appConfig$Environment=="Production",appConfig$applicationURL,'')
 
-sideBarMenuItems <- read_tsv('./config/sideBarMenuItems.tsv') %>% filter(IsHidden == 0) %>% select(-c(IsHidden))
+sideBarMenuItems <- read_tsv('./config/sidebarMenuItems.tsv',col_types = cols()) %>% filter(IsHidden == 0) %>% select(-c(IsHidden))
 
 namespaces <- as.list(sideBarMenuItems$tabName) 
 
 tabs <- as.list(sideBarMenuItems$tabName)    
 
-dropdownlinks <- read_tsv("./config/dropdownlinks.tsv")
+dropdownlinks <- read_tsv("./config/dropdownlinks.tsv",col_types = cols())
 
-tutorials <- read_tsv("./config/tutorials.tsv")
+tutorials <- read_tsv("./config/tutorials.tsv",col_types = cols())
 
 plotlyCustomIcons <- readRDS('config/plotlyCustomIcons.rds')
 
@@ -24,9 +24,5 @@ statTests <- CUSOMShinyHelpers::getStatTestByKeyGroup.methods
 adjustmentMethods <- c("none","Bonferroni","Benjamini-Hochberg (FDR)")
 
 pValueThreshold <- 0.05
-
-
-
-set.seed(round(runif(1,0,1)*1000000))
 
 
